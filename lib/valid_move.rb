dd96216922be9c5c6ab6a915df9932d50aa646cf
boolean-messaging-0131 <evankrone1@gmail.com>
2019-07-28 18:49:54 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,5 +1,6 @@
 # code your #valid_move? method here
-def valid_move?
+def valid_move?(baord, index)
+  if index < 0 
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 
